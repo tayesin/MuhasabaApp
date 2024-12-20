4336dad27e1df20139d636da8f4321853de96873
Tahasin Rahman <tayesin@gmail.com>
2024-12-19 19:08:18 -0800

    Initial Commit

---
+++ MuhasabaApp/MuhasabaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuhasabaAppApp.swift
+//  MuhasabaApp
+//
+//  Created by Tahasin Rahman on 12/19/24.
+//
+
+import SwiftUI
+
+@main
+struct MuhasabaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
